9226525129d7c866fccd485aa5d6785e9d80ac23
g-susvs <jesus.savs19@gmail.com>
2023-07-24 23:22:26 -0500

    Montando nginx.conf

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 
-const categoryRoutes = require("./components/category/routes");
-
 const { dbConnect } = require("./database/connect");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,8 +9,7 @@ dbConnect();
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Proyecto servicios"));
-app.use("/api/category", categoryRoutes);
+app.get("/", (req, res) => res.send("Proyecto servicios!!"));
 
 app.listen(port, () => {
   console.log(`Servicios corriendo en http://localhost:${port}`);
